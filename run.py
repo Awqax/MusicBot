b8ca85f9bb54c20f2a5c7389adece85176941eba
Alideew1234 <33075769+Alideew1234@users.noreply.github.com>
2018-09-21 20:46:57 +0100

    Update run.py

--- run.py
+++ run.py
@@ -290,9 +290,6 @@ def req_ensure_encoding():
             sys.stdout.isatty = lambda: True
 
 
-def req_ensure_env():
-    log.info("Ensuring we're in the right environment")
-
     if os.environ.get('APP_ENV') != 'docker' and not os.path.isdir(b64decode('LmdpdA==').decode('utf-8')):
         log.critical(b64decode('Qm90IHdhc24ndCBpbnN0YWxsZWQgdXNpbmcgR2l0LiBSZWluc3RhbGwgdXNpbmcgaHR0cDovL2JpdC5seS9tdXNpY2JvdGRvY3Mu').decode('utf-8'))
         bugger_off()
